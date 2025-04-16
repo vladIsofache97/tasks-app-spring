815bf8e60629dc88c1a59718784fc02347b7c23d
Isofache Vlad <vlad.isofache1997@gmail.com>
2025-04-16 19:48:41 +0300

    first commit

---
+++ src/test/java/com/iso/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iso.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
